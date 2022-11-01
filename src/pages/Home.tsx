3b1da792a88ff9603f9933d10a8e7ee8dada6dee
josepps <joseppsantanafs@gmail.com>
2022-11-01 18:30:53 -0300

    Card Pokemon criado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,37 @@ function Home() {
         <>
     <NavBar />
     <h1 className="title">Encontre todos os pokémons em um só lugar</h1>
+
+    <div className="list">
+        <div className="card">
+            <div className="info">
+                <span className="info__number">#001</span>
+                <p className="info__name">Bubassauro</p>
+            </div>
+            <img className="card__img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+        </div>
+        <div className="card">
+            <div className="info">
+                <span className="info__number">#001</span>
+                <p className="info__name">Bubassauro</p>
+            </div>
+            <img className="card__img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+        </div> 
+        <div className="card">
+            <div className="info">
+                <span className="info__number">#001</span>
+                <p className="info__name">Bubassauro</p>
+            </div>
+            <img className="card__img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+        </div> 
+        <div className="card">
+            <div className="info">
+                <span className="info__number">#001</span>
+                <p className="info__name">Bubassauro</p>
+            </div>
+            <img className="card__img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+        </div>
+    </div>
     </>
     );
 }
