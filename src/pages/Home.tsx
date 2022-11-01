1db49c07bce404f42105c14c9ddf582dd3301ae1
josepps <joseppsantanafs@gmail.com>
2022-11-01 17:59:07 -0300

    Titulo da Home feito

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import NavBar from "../components/NavBar";
+import './Home.css'
 
 function Home() {
-    return <NavBar />
+    return (
+        <>
+    <NavBar />
+    <h1 className="title">Encontre todos os pokémons em um só lugar</h1>
+    </>
+    );
 }
 
 export default Home;
