58ad5caaec079e69214858b22b7f342bbb9add3a
josepps <joseppsantanafs@gmail.com>
2022-11-01 22:52:54 -0300

    Componente de Card criado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,19 @@
+import CardPokemon from "../components/CardPokemon";
 import NavBar from "../components/NavBar";
-import './Home.css'
+import './Home.css';
+import pokemonList from "../assets/pokemon.json"
 
 function Home() {
+    const { pokemons } = pokemonList;
     return (
         <>
     <NavBar />
     <h1 className="title">Encontre todos os pokémons em um só lugar</h1>
 
     <div className="list">
-        <div className="card">
-            <div className="info">
-                <span className="info__number">#001</span>
-                <p className="info__name">Bubassauro</p>
-            </div>
-            <img className="card__img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-        </div>
-        <div className="card">
-            <div className="info">
-                <span className="info__number">#001</span>
-                <p className="info__name">Bubassauro</p>
-            </div>
-            <img className="card__img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-        </div> 
-        <div className="card">
-            <div className="info">
-                <span className="info__number">#001</span>
-                <p className="info__name">Bubassauro</p>
-            </div>
-            <img className="card__img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-        </div> 
-        <div className="card">
-            <div className="info">
-                <span className="info__number">#001</span>
-                <p className="info__name">Bubassauro</p>
-            </div>
-            <img className="card__img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-        </div>
+        {pokemons.map((pokemon) => (
+        <CardPokemon key={pokemon.id} id={pokemon.id} name={pokemon.name} />
+        ))}
     </div>
     </>
     );
